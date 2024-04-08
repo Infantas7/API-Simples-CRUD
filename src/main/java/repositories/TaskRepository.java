ef348483060fcbf4ff31d14a1c6b6f7834fc91d3
Infantas <gabriel652010@hotmail.com>
2024-04-08 20:40:04 -0300

    refactor TaskRepository

--- src/main/java/repositories/TaskRepository.java
+++ src/main/java/repositories/TaskRepository.java
@@ -2,9 +2,11 @@ package repositories;
 
 import models.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TaskRepository extends JpaRepository<Task, Integer> {
 
 
